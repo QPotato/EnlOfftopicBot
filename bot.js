f646e49cfbb58d5dcde59e8673809067b50b30ae
QPotato <federicobadaloni@hotmail.com>
2019-06-05 16:17:04 -0300

    New features

--- bot.js
+++ bot.js
@@ -1,26 +1,68 @@
 var fs = require('fs');
 const TelegramBot = require('node-telegram-bot-api');
+const Request = require("request");
 
-// replace the value below with the Telegram token you receive from @BotFather
+
+// Telegram token
 const token = fs.readFileSync('TOKEN', 'utf8').trim();
-
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-const p = 0.01;
-// Listen for any kind of message. There are different kinds of
-// messages.
+const p = 0.05;
+
+// Estadisticasbcra token
+const brcatoken = fs.readFileSync('BCRA', 'utf8').trim();
+
 
 bot.onText(/\/cancionero/, (msg) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-  console.log("Hola");
+  // Send a random ENL song
   const chatId = msg.chat.id;
-  cancion = canciones[Math.floor(Math.random()*canciones.length)];
+  const cancion = canciones[Math.floor(Math.random()*canciones.length)];
 
   console.log(cancion);
   bot.sendMessage(chatId, cancion, {parse_mode: "Markdown"});
+});
+
+bot.onText(/\/redditpics (.+)/, (msg, match) => {
+  // Send random pics from last posts on given subreddit.
+  const chatId = msg.chat.id;
+  const url = "https://www.reddit.com/r/" + match[1] + "/hot.json";
+  Request.get(url, (error, response, body) => {
+    if(error) {
+      return console.log(error);
+    }
+    const sub = JSON.parse(body);
+    if('data' in sub) {
+      const posts = sub.data.children;
+      const links = posts
+                .filter((p) => p.kind === "t3")
+                .map((p) => p.data.url)
+  
+      bot.sendMessage(chatId, links[Math.floor(Math.random()*links.length)]);
+    }
+  });
+});
+
+bot.onText(/\/macrisis/, (msg) => {
+  const chatId = msg.chat.id;
+  const url = "https://api.estadisticasbcra.com/usd_of_minorista";
+  Request.get(url, {headers: {Authorization: "BEARER " + brcatoken}}, (error, response, body) => {
+    if(error) {
+      return console.log(error);
+    }
+    const cotizaciones = JSON.parse(body);
+    dolar = cotizaciones[cotizaciones.length - 1].v
+
+    const url2 = "https://api.estadisticasbcra.com/tasa_leliq"; 
+    Request.get(url2, {headers: {Authorization: "BEARER " + brcatoken}}, (error, response, body) => {
+      if(error) {
+        return console.log(error);
+      }
+      const tasas = JSON.parse(body);
+      leliq = tasas[tasas.length - 1].v
+  
+      bot.sendMessage(chatId, `Dolar: $${dolar}\nLELIQs: ${leliq}%`);
+    });
+  });
 });
 
 
@@ -56,22 +98,26 @@ bot.on('new_chat_members', (msg) => {
 
 
 const respuestas_especificas = {
-  "potusito": ["No te respetas..."],
-  "caisaros": ["Callate Perez."],
-  "pansitopan": ["No, no, no. Bueno, sí.", "Cereal."],
-  "quanticpotato": ["Tiene razón.", "Mi creador habla con sabiduría nuevamente."],
+  "potusito": ["No te respetas...", "No te estan regando lo suficiente, Potus."],
+  "caisaros": ["Callate Perez.", "Como las vacas en Perez?", "Frodo tiene razon."],
+  "pansitopan": ["No, no, no. Bueno, sí.", "mmm, cereal."],
+  "quanticpotato": ["Tiene razón.", "Mi creador habla con sabiduría nuevamente.", "Escuchen a Fede, tiene razon.", "Yo estoy de acuerdo."],
+  "dimekari": ["Hola hermosa.", "Pasame pack Kari, quiero ver ese codigo.", "Apa, una linda bot. Hola, me llamo EnlOfftopicBot. Queres ver mi codigo?"]
 };
 
 const respuestas_random = [
   "Send nudes BB.",
+  "Sí.",
   "No.",
+  "Puede ser, eh..",
   "Y si jugás un poco de Ingress? Digo nomás...",
   "Y como viene el score de tu celda?",
   "Vos decís?",
   "O sea, técnicamente tenés razón, pero como lo decís vos probablemente sea una pavada.",
   "Esto es importante!",
   "Después de leer esto, quiero que alguien me apague. Para siempre.",
-  "No te creo."
+  "No te creo.",
+  "Miren que leí muchas pavadas en este grupo pero esto..."
 ]
 
 const mensajes_idle = [
