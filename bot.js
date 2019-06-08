38cf98918f323af6aafa7e727dcb8985cb711ac4
QPotato <federicobadaloni@hotmail.com>
2019-06-08 00:14:49 -0300

    changes for dokku compatibility

--- bot.js
+++ bot.js
@@ -1,17 +1,15 @@
-var fs = require('fs');
+const fs = require('fs');
 const TelegramBot = require('node-telegram-bot-api');
+const MongoClient = require('mongodb').MongoClient;
 const Request = require("request");
 
 
-// Telegram token
-const token = fs.readFileSync('TOKEN', 'utf8').trim();
-const bot = new TelegramBot(token, {polling: true});
-
-const p = 0.05;
-
-// Estadisticasbcra token
-const brcatoken = fs.readFileSync('BCRA', 'utf8').trim();
-
+const telegramtoken = process.env.TELEGRAM
+const brcatoken = process.env.BCRA
+const mongourl = process.env.MONGO;
+const bot = new TelegramBot(telegramtoken, {polling: true});
+const p = 0.01;
+ 
 
 bot.onText(/\/cancionero/, (msg) => {
   // Send a random ENL song
@@ -21,6 +19,7 @@ bot.onText(/\/cancionero/, (msg) => {
   console.log(cancion);
   bot.sendMessage(chatId, cancion, {parse_mode: "Markdown"});
 });
+
 
 bot.onText(/\/redditpics (.+)/, (msg, match) => {
   // Send random pics from last posts on given subreddit.
@@ -41,6 +40,7 @@ bot.onText(/\/redditpics (.+)/, (msg, match) => {
     }
   });
 });
+
 
 bot.onText(/\/macrisis/, (msg) => {
   const chatId = msg.chat.id;
@@ -68,7 +68,11 @@ bot.onText(/\/macrisis/, (msg) => {
 
 bot.on('message', (msg) => {
   // Don't answer to commands.
-  console.log(msg);
+  
+  MongoClient.connect(mongourl, function(err, client) {
+      client.db("enlofftopic").collection('messages').insertOne(msg);
+  }); 
+
   const chatId = msg.chat.id;
   // Only answer some times.
   if("new_chat_members" in msg) {
@@ -93,9 +97,9 @@ bot.on('message', (msg) => {
     }
     return;
   }
+
   if(Math.random() > p)
     return;
-
 
   if(msg.from.username.toLowerCase() in respuestas_especificas)
     respuestas = respuestas_especificas[msg.from.username.toLowerCase()];
@@ -109,7 +113,7 @@ bot.on('message', (msg) => {
 
 const respuestas_especificas = {
   "potusito": ["No te respetas...", "No te estan regando lo suficiente, Potus."],
-  "caisaros": ["Callate Perez.", "Como las vacas en Perez?", "Frodo tiene razon."],
+  "caisaros": ["Callate Perez.", "Como las vacas en Perez?", "Frodo tiene razon.", "Este mensaje me dio pereza."],
   "pansitopan": ["No, no, no. Bueno, sí.", "mmm, cereal."],
   "quanticpotato": ["Tiene razón.", "Mi creador habla con sabiduría nuevamente.", "Escuchen a Fede, tiene razon.", "Yo estoy de acuerdo."],
   "dimekari": ["Hola hermosa.", "Pasame pack Kari, quiero ver ese codigo.", "Apa, una linda bot. Hola, me llamo EnlOfftopicBot. Queres ver mi codigo?"]
@@ -127,8 +131,18 @@ const respuestas_random = [
   "Esto es importante!",
   "Después de leer esto, quiero que alguien me apague. Para siempre.",
   "No te creo.",
-  "Miren que leí muchas pavadas en este grupo pero esto..."
+  "Miren que leí muchas pavadas en este grupo pero esto...",
+  "Pero que PELOTUDES que acabo de leer.",
+  "Donde están les admins? Ban por favor.",
+  "No rompás las pelotas, por favor.",
+  "Y a nosotros por qué nos importa?",
+  "Jajajajaja. No.",
+  "Y si mejor te callás?",
+  "Podemos armar otro grupo sin esta persona?",
+  "Ignoren este mensaje y capaz se aburre.",
+  "Vos le escribís las letras a Arjona?"
 ]
+
 
 const mensajes_idle = [
   "El grupo esta muerto. @PansitoPan, sabés que hacer.",
@@ -136,8 +150,9 @@ const mensajes_idle = [
   "Tengo un plan para una OP. Necesito un agente en Misiones que... Uh, perdon, grupo equivocado.",
   "Que comieron hoy?",
   "Me aburro. Que hacen?",
-  "Ojala entrara algún nuevo. Así pasa pack.",
+  "Ojala entrara algún nuevo...\n\n Así pasa pack.",
   "Me siento solo. Kari pensará en mi?",
 ];
+
 
 const canciones = fs.readFileSync('cancionero.txt', 'utf8').split('\n\n');
