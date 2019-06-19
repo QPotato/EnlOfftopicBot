df2f679d5bc96a02430a517b8e43dabd3758c983
QPotato <federicobadaloni@hotmail.com>
2019-06-18 23:14:20 -0300

    db module

--- src/bot.ts
+++ src/bot.ts
@@ -1,40 +1,38 @@
 import * as TelegramBot from 'node-telegram-bot-api';
-import * as MongoClient from 'mongodb';
 
+import mongo from './db'
 import commands from './commands';
 import reactions from './reactions'
 
-const telegramtoken = process.env.TELEGRAM || "NoHayToken";
-const mongourl = process.env.MONGO|| "NoHayMongo";
-const bot = new TelegramBot(telegramtoken, { polling: true });
+if (process.env.TELEGRAM === undefined)
+  throw new Error('No telegram token in environment')
+const telegramtoken = process.env.TELEGRAM;
 
-
-// Bot should answer as a command if the text matches a command pattern.
-commands.forEach((cmd) => {
-  bot.onText(cmd.pattern, cmd.action);
-});
-
-bot.on('message', (msg: TelegramBot.Message) => {
-  // We store all messages.
-  MongoClient.connect(mongourl, function (err, client) {
-    client.db("enlofftopic").collection('messages').insertOne(msg);
+const init = async () => {
+  // Connect the DB
+  await mongo.connect();
+  if (mongo.db === undefined)
+    throw new Error('Could not connect to mongodb')
+  const bot = new TelegramBot(telegramtoken, { polling: true });
+  
+  // Bot should answer as a command if the text matches a command pattern.
+  commands.forEach((cmd) => {
+    bot.onText(cmd.pattern, cmd.action);
   });
 
-  // React to a message with the first that matches the pattern.
-  for(const reaction of reactions) {
-    if(reaction.pattern(msg)) {
-      reaction.action(msg);
-      break;
-    }
-  }
-});
+  bot.on('message', (msg: TelegramBot.Message) => {
+    // We store all messages.
+    mongo.db.collection('messages').insertOne(msg);
 
-// const mensajes_idle = [
-//   "El grupo esta muerto. @PansitoPan, sabés que hacer.",
-//   "Che, yo no puedo generar spam nuevo. Soy un bot. Mis respuestas son todas preprogramadas. Si ustedes no hablan el grupo muere.",
-//   "Tengo un plan para una OP. Necesito un agente en Misiones que... Uh, perdon, grupo equivocado.",
-//   "Que comieron hoy?",
-//   "Me aburro. Que hacen?",
-//   "Ojala entrara algún nuevo...\n\n Así pasa pack.",
-//   "Me siento solo. Kari pensará en mi?",
-// ];
+    // React to a message with the first that matches the pattern.
+    for(const reaction of reactions) {
+      if(reaction.pattern(msg)) {
+        reaction.action(msg);
+        break;
+      }
+    }
+  });
+}
+
+init()
+
---
+++ src/db.ts
@@ -0,0 +1,19 @@
+import { MongoClient, Db } from "mongodb";
+
+if (process.env.MONGO === undefined)
+  throw new Error('No mongo url in environment')
+  
+const mongourl = process.env.MONGO;
+class DbClient {
+
+    public db!: Db;
+
+    public async connect() {
+        let client = await MongoClient.connect(mongourl, { useNewUrlParser: true })
+        this.db = client.db('enlofftopictest') ;
+        return this.db;
+    }
+}
+
+const mongo = new DbClient();
+export default mongo;
